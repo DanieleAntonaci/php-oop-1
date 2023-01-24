de9db62940e57401b67256d6c3dadb045b93aef1
DanieleAntonaci <danieleanto96@gmail.com>
2023-01-24 17:16:31 +0100

    i generi accettano un array

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
 class Movie{
     public $title;
-    public $genre;
+    public array $genre;
     public $cast;
     public $duration;
     public $year;
 
-    public function __construct($title, $genre, $cast, $duration, $year)
+    public function __construct($title, array $genre, $cast, $duration, $year)
     {
         $this->title =$title;
         $this->genre =$genre;
@@ -22,8 +22,11 @@ class Movie{
 
     public function getHtml(){
 
+        $allgenre= implode(", ", $this->genre);
+
+
         return '<h1>name movie: ' . $this->title . '</h1>'
-            . '<h2> movie genre: ' . $this->genre . '</h2>'
+            . '<h2> movie genre: ' . $allgenre . '</h2>'
             . '<h3> cast: ' . $this-> cast . '</h3>'
             . '<h3> duration: ' . $this-> convertHour() . ' hour</h3>'
             . '<h3> anno: ' . $this -> year . '</3>'
@@ -33,8 +36,29 @@ class Movie{
 
 
 $movies = [
-    ['Avengers: Endgame', 'fantasy','Robert Downey Jr., Chris Evans', '181', 2019],
-    ['Avatar - La via dell`acqua', 'fantasy',' Sam Worthington', '192', 2022]
+    [
+        'Avengers: Endgame', 
+        [
+            'Fantasy',
+            'Action',
+            'Adventure'
+        ],
+        'Robert Downey Jr., 
+        Chris Evans', 
+        '181', 
+        2019
+    ],
+    [
+        'Avatar - La via dell`acqua', 
+        [
+            'Fantasy',
+            'Action',
+            'Adventure'
+        ],
+        'Sam Worthington', 
+        '192', 
+        2022
+    ]
 ];
 
 
@@ -42,5 +66,4 @@ $movies = [
 foreach($movies as $movie){
     $newMovie = new Movie($movie[0], $movie[1], $movie[2], $movie[3], $movie[4]);
     echo $newMovie->getHtml();
-
 }
